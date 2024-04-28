f2bc0c8551382336adccfb34935f79009f0b45b3
termuxdev314 <termuxdev314@gmail.com>
2024-04-28 16:25:19 +0200

    Create

---
+++ README.md
@@ -0,0 +1,8 @@
+# pypa
+
+pypa is a tool written in Bash that allows you to create Python modules without much effort. It creates the basic folder structure of a working Python module as required by PyPi.org. Especially beginners or those who are creating their first module often have difficulties creating a running module that is imported correctly. The tool helps to create the structure and get the module running. This program has the following features:
+1. Creates the basic structure
+2. Adds submodules and creates the __init__.py directly
+3. When the module is ready, the module is initialized using the "init" command. A basic setup.py is added and the main __init__.py is created
+4. The build and upload is integrated
+
---
+++ scripts/install-deps.bash
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+LINUX="$1"
+MANAGER="$2"
+
+setup_debian() {
+	apt-get update -qq
+	apt-get install -y python3 python3-pip python3-venv pipx rust
+	pipx install twine
+	python3 -m pip install build
+}
+
+setup_archlinux() {
+	pacman -Syy
+	pacman --noconfirm --Sync python3 python3-pip pipx rust
+	pipx install twine
+	python3 -m pip install build
+}
+
+setup_termux_apt() {
+	apt-get update -qq
+	apt-get install -y python python-pip rust wget
+	wget https://github.com/termuxdev314/termuxdev314.github.io/releases/download/v.1.0/python-repo.deb
+	dpkg -i python-repo.deb
+	rm -rf python-repo.deb
+	apt-get update -qq
+	apt-get install -y python-twine
+	python -m pip install build
+}
+
+setup_termux_pacman() {
+	pacman -Syy
+	pacman --noconfirm -Sync python python-pip rust wget dpkg apt
+	mkdir -p ${PREFIX}/etc/apt/sources.list.d/
+	mkdir -p ${PREFIX}/etc/apt/preferences.d/
+	mkdir -p ${PREFIX}/etc/apt/apt.conf.d/
+	wget https://github.com/termuxdev314/termuxdev314.github.io/releases/download/v.1.0/python-repo.deb
+	dpkg -i python-repo.deb
+	rm -rf python-repo.deb
+	apt-get update -qq
+	apt-get install -y python-twine
+	python -m pip install build
+}
+
+case "${LINUX}" in
+	linux|--linux|-l)
+		case "${MANAGER}" in
+			apt|--apt|-a)
+				setup_debian ;;
+			pacman|--pacman|-p)
+				setup_archlinux ;;
+			*)
+				echo "Wrong Manager" ;;
+		esac
+		;;
+	termux|--termux|-t)
+		case "${MANAGER}" in
+			apt|--apt|-a)
+				setup_termux_apt ;;
+			pacman|--pacman|-p)
+				setup_termux_pacman ;;
+			*)
+				echo "Wrong Manager" ;;
+		esac
+		;;
+	-la)
+		setup_debian ;;
+	-lp)
+		setup_archlinux ;;
+	-ta)
+		setup_termux_apt ;;
+	-tp)
+		setup_termux_pacman ;;
+	*)
+		echo "Wrong Operant" ;;
+esac
