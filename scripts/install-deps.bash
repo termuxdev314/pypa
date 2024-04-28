b1ae6eee6fa9518daf829c99c8d8469f2fef15c6
termuxdev314 <termuxdev314@gmail.com>
2024-04-28 16:55:52 +0200

    Update setup script

--- scripts/install-deps.bash
+++ scripts/install-deps.bash
@@ -5,21 +5,21 @@ MANAGER="$2"
 
 setup_debian() {
 	apt-get update -qq
-	apt-get install -y python3 python3-pip python3-venv pipx rust
+	apt-get install -y python3 python3-pip python3-venv pipx rust build-essential
 	pipx install twine
 	python3 -m pip install build
 }
 
 setup_archlinux() {
 	pacman -Syy
-	pacman --noconfirm --Sync python3 python3-pip pipx rust
+	pacman --noconfirm --Sync python3 python3-pip pipx rust base-devel
 	pipx install twine
 	python3 -m pip install build
 }
 
 setup_termux_apt() {
 	apt-get update -qq
-	apt-get install -y python python-pip rust wget
+	apt-get install -y python python-pip rust wget build-essential
 	wget https://github.com/termuxdev314/termuxdev314.github.io/releases/download/v.1.0/python-repo.deb
 	dpkg -i python-repo.deb
 	rm -rf python-repo.deb
@@ -30,7 +30,7 @@ setup_termux_apt() {
 
 setup_termux_pacman() {
 	pacman -Syy
-	pacman --noconfirm -Sync python python-pip rust wget dpkg apt
+	pacman --noconfirm -Sync python python-pip rust wget dpkg apt base-devel
 	mkdir -p ${PREFIX}/etc/apt/sources.list.d/
 	mkdir -p ${PREFIX}/etc/apt/preferences.d/
 	mkdir -p ${PREFIX}/etc/apt/apt.conf.d/
